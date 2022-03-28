9b8850db47c2b4029c5237f9aa8de7bee99aa521
Cristhian Valencia <valenciacristhian47@gmail.com>
2022-03-28 00:22:31 -0300

    add recipe detail view

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -49,3 +49,11 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Create a new recipe"""
         serializer.save(user=self.request.user)
+
+    def get_serializer_class(self):
+        """Return appropriate serializer class"""
+        if self.action == 'retrieve':
+            return serializers.RecipeDetailSerializer
+        elif self.action == 'upload_image':
+            return serializers.RecipeImageSerializer
+        return self.serializer_class
